03485752775d2d9f5e40664f6995c25204d185be
negative-zer0 <iunixpl69@gmail.com>
2020-10-01 17:53:22 +0200

    first commit

---
+++ src/main/java/net/unix/proctor/ApplicationContext.java
@@ -0,0 +1,14 @@
+package net.unix.proctor;
+
+/**
+ * @author Unix
+ * @since 01.10.2020
+ */
+
+public class ApplicationContext
+{
+    public static void main(String... args) throws Throwable {
+        new Proctor()
+                .onStart();
+    }
+}
