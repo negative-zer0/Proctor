01fe7cb69bb875fd6a4c021eafcd0194ee16d264
negative-zer0 <iunixpl69@gmail.com>
2021-03-29 16:02:12 +0200

    some changes

--- src/main/java/net/unix/proctor/Proctor.java
+++ src/main/java/net/unix/proctor/Proctor.java
@@ -1,51 +1,51 @@
 package net.unix.proctor;
 
+import net.unix.proctor.util.Pair;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.text.DecimalFormat;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
 
 /**
  * @author Unix
  * @since 01.10.2020
  */
 
-public class Proctor
-{
+public class Proctor {
+
     private final DecimalFormat decimalFormat = new DecimalFormat("##.##");
 
     public void onStart() throws Throwable {
         final File file = new File("images");
-        if (!file.exists() && file.mkdirs())
+
+        if (!file.exists() && file.mkdirs()) {
             throw new UnsupportedOperationException("Created images file. Put images with name 1 and 2 with format .jpg in folder images");
-
-        final BufferedImage image1 = ImageIO.read(new File("images/1.jpg")),
-                            image2 = ImageIO.read(new File("images/2.jpg"));
-
-        final int width1 = image1.getWidth(),
-                  height1 = image1.getHeight(),
-                  width2 = image2.getWidth(),
-                  height2 = image2.getHeight();
-
-        if (width1 != width2 && height1 != height2)
-            throw new UnsupportedOperationException("Both images should have same dimensions");
-
-        long diff = 0;
-        for (int i = 0; i < height1; ++i) {
-            for (int j = 0; j < width1; ++j) {
-                final Color color1 = new Color(image1.getRGB(j, i), true),
-                            color2 = new Color(image2.getRGB(j, i), true);
-                diff += (Math.abs(color1.getRed() - color2.getRed())
-                        + Math.abs(color1.getGreen() - color2.getGreen())
-                        + Math.abs(color1.getBlue() - color2.getBlue())
-                );
-            }
         }
 
-        final double average = diff / (width1 * height2 * 3.D);
-        final double percentage = (average / 255.D) * 100.D;
+        final Pair<BufferedImage, BufferedImage> pair = new Pair<>(ImageIO.read(new File("images/1.jpg")), ImageIO.read(new File("images/2.jpg")));
+        final Pair<Integer, Integer> width = new Pair<>(pair.getFirst().getWidth(), pair.getSecond().getWidth()),
+                height = new Pair<>(pair.getFirst().getHeight(), pair.getSecond().getHeight());
+
+        if (!width.getFirst().equals(width.getSecond()) && !height.getFirst().equals(height.getSecond())) {
+            throw new UnsupportedOperationException("Both images should have same dimensions");
+        }
+
+        final AtomicLong diff = new AtomicLong();
+        IntStream.range(0, height.getFirst()).forEachOrdered(i -> IntStream.range(0, width.getFirst())
+                .mapToObj(j -> new Pair<>(new Color(pair.getFirst().getRGB(j, i), true), new Color(pair.getSecond().getRGB(j, i), true)))
+                .forEach(color -> diff.getAndAdd((Math.abs(color.getFirst().getRed() - color.getSecond().getRed())
+                        + Math.abs(color.getFirst().getGreen() - color.getSecond().getGreen())
+                        + Math.abs(color.getFirst().getBlue() - color.getSecond().getBlue())
+                )))
+        );
+
+        final double average = diff.get() / (width.getFirst() * height.getSecond() * 3.0D);
+        final double percentage = (average / 255.0D) * 100.0D;
         System.out.println("Average: " + decimalFormat.format(average));
         System.out.println("Diffrence: " + decimalFormat.format(percentage) + "%");
     }
+
 }
